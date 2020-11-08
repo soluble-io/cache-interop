7e725e851e9b71579007e8c9cdd9f836feffec2a
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-11-08 20:51:14 +0100

    fix: node env

--- packages/cache-interop/jest.config.js
+++ packages/cache-interop/jest.config.js
@@ -4,19 +4,17 @@ module.exports = {
   name: 'unit',
   displayName: 'unit',
   verbose: true,
-  collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts'],
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts', , '!src/**/*.test.ts'],
   moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'json', 'node'],
   //roots: ['<rootDir>/../../', '<rootDir>/../../'],
   testMatch: ['<rootDir>/test/**/*.{spec,test}.{ts,js}', '<rootDir>/src/**/*.{spec,test}.{ts,js}'],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transform: {
     '^.+\\.(js|ts)$': 'ts-jest',
   },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$', '^.+\\.module\\.(css|sass|scss|less)$'],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|ts)$'],
   modulePaths: [],
-
   globals: {
-    window: {},
     'ts-jest': {
       diagnostics: true,
       tsconfig: './tsconfig.jest.json',
