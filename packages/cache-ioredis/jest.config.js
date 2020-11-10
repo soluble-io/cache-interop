180207ab5d1bd5bab6dc4955aab45f24ec77e5a0
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-11-10 12:36:55 +0100

    try out new configuration

--- packages/cache-ioredis/jest.config.js
+++ packages/cache-ioredis/jest.config.js
@@ -1,19 +1,30 @@
-'use strict';
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { defaults: tsjPreset } = require('ts-jest/presets');
+const { compilerOptions } = require('./tsconfig.jest.json');
 
+console.log('cool', pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }));
+
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   name: 'cache-ioredis/unit',
   displayName: 'cache-ioredis/unit',
+  testRunner: 'jest-circus/runner',
   verbose: true,
   collectCoverageFrom: ['src/**/*.{js,ts}', '!src/**/*.d.ts', '!src/**/*.test.ts'],
   moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'json', 'node'],
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   testEnvironment: 'node',
+  rootDir: '.',
+  //moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  moduleNameMapper: {
+    '^@soluble/cache\\-interop$': '../cache-interop/src',
+  },
   transform: {
     '^.+\\.(js|ts)$': 'ts-jest',
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|ts)$'],
   modulePaths: [],
-
   globals: {
     'ts-jest': {
       diagnostics: true,
