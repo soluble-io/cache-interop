f349070e6d5e30f44353faca0241b98bdb3c89e6
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-11-23 13:37:39 +0100

    remove utc date support

--- packages/cache-interop/src/expiry/date-provider.interface.ts
+++ packages/cache-interop/src/expiry/date-provider.interface.ts
@@ -2,11 +2,6 @@ import { UnixTime } from './es-date-provider';
 
 export interface DateProvider {
   /**
-   * @param date
-   */
-  convertDateToUnixTime(date: Date): UnixTime;
-
-  /**
    * Return the current time as Unix Timestamp (number of seconds since January 1, 1970)
    */
   getUnixTime(): UnixTime;
--- packages/cache-interop/src/expiry/es-date-provider.ts
+++ packages/cache-interop/src/expiry/es-date-provider.ts
@@ -3,10 +3,6 @@ import { DateProvider } from './date-provider.interface';
 export type UnixTime = number;
 
 export class EsDateProvider implements DateProvider {
-  convertDateToUnixTime(date: Date): UnixTime {
-    return Math.floor(date.getTime() / 1000);
-  }
-
   getUnixTime(): UnixTime {
     return Math.floor(Date.now() / 1000);
   }
