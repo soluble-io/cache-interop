0bf63ce6116dbe91413d8d1307dca7d986651e94
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-12-04 23:29:10 +0100

    feat: add internal utils for setting default options

---
+++ packages/cache-interop/src/utils/cache-options-utils.ts
@@ -0,0 +1,25 @@
+import { GetOrSetOptions } from '../cache.interface';
+
+type GetOrSetCacheDisabledParams = {
+  read: boolean;
+  write: boolean;
+};
+
+/**
+ * @internal
+ */
+export const getGetOrSetCacheDisabledParams = (
+  disableCache: GetOrSetOptions['disableCache'] = false
+): GetOrSetCacheDisabledParams => {
+  const disabled: GetOrSetCacheDisabledParams =
+    typeof disableCache === 'boolean'
+      ? {
+          write: disableCache,
+          read: disableCache,
+        }
+      : {
+          write: disableCache.write,
+          read: disableCache.read,
+        };
+  return disabled;
+};
