0c4c89a5d470875ca743b52a5205647491770046
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-12-29 21:24:52 +0100

    feat: export ConnectionInterface and narrow type exports

--- packages/cache-interop/src/index.ts
+++ packages/cache-interop/src/index.ts
@@ -1,9 +1,11 @@
 export * from './cache.interface';
-export * from './cache-item.interface';
-export * from './cache-item';
+export type { CacheItemInterface } from './cache-item.interface';
+export { CacheItem } from './cache-item';
 export * from './adapter';
 export * from './exceptions';
 export * from './serializer';
 export * from './types';
 export * from './utils';
 export * from './eviction';
+export type { ConnectionInterface } from './connection/connection.interface';
+export { NullConnection } from './connection/null-connection';
