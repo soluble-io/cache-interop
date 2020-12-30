3f9f03ed387e461ab61fc24453af55d66b5f3309
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-12-30 23:44:31 +0100

    tests(unit): added mini suite for invalid cache keys

---
+++ packages/cache-interop/src/adapter/__tests__/map-cache-adapter.test.ts
@@ -0,0 +1,14 @@
+import { MapCacheAdapter } from '../map-cache-adapter';
+import { InvalidCacheKeyException } from '../../exceptions';
+
+describe('MapCacheAdapter tests', () => {
+  describe('When cache key is invalid', () => {
+    const cache = new MapCacheAdapter();
+    const key = ({ errKey: true } as unknown) as string;
+    it('should return InvalidCacheKeyException for simple operations', async () => {
+      await expect(cache.delete(key)).resolves.toBeInstanceOf(InvalidCacheKeyException);
+      await expect(cache.set(key, 'cool')).resolves.toBeInstanceOf(InvalidCacheKeyException);
+      await expect(cache.delete(key)).resolves.toBeInstanceOf(InvalidCacheKeyException);
+    });
+  });
+});
