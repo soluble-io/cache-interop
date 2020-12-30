87b87af9780ef0d4d3316860610b346707f1d10c
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-12-30 23:43:08 +0100

    feat: add new exports

--- packages/cache-interop/src/index.ts
+++ packages/cache-interop/src/index.ts
@@ -1,14 +1,24 @@
 export * from './cache.interface';
+
+// Cache Items
 export type { CacheItemInterface } from './cache-item.interface';
 export { CacheItem } from './cache-item';
+export { CacheItemFactory } from './cache-item.factory';
+
 export * from './adapter';
 export * from './exceptions';
 export * from './serializer';
 export * from './types';
 export * from './utils';
 export * from './eviction';
+
+// Connection
+
 export type { ConnectionInterface } from './connection/connection.interface';
 export type { ConnectedAdapterInterface } from './connection/connected-adapter.interface';
 export { isConnectedAdapter } from './connection/is-connected-adapter';
-
 export { NullConnection } from './connection/null-connection';
+
+// Typeguards and Assertions
+export { Guards } from './validation/guards';
+export { Asserts } from './validation/asserts';
