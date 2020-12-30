1d9aca297a8a0529103190d997e4333a0f7f9685
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-12-30 23:42:50 +0100

    feat(factory): added convenience cache-item factories

---
+++ packages/cache-interop/src/__tests__/cache-item.factory.test.ts
@@ -0,0 +1,65 @@
+import { CacheException, InvalidCacheKeyException } from '../exceptions';
+import { CacheItemFactory } from '../cache-item.factory';
+
+describe('CacheItemFactory', () => {
+  describe('fromOk', () => {
+    it('should create a success item', () => {
+      const item = CacheItemFactory.fromOk<string>({
+        key: 'k',
+        data: 'hello',
+        isHit: true,
+        isPersisted: false,
+      });
+      expect(item).toMatchObject({
+        isSuccess: true,
+        data: 'hello',
+        isHit: true,
+        isPersisted: false,
+        metadata: {
+          key: 'k',
+        },
+        error: null,
+      });
+    });
+  });
+
+  describe('fromInvalidCacheKey', () => {
+    it('should create an error with invalid key', () => {
+      const item = CacheItemFactory.fromInvalidCacheKey<string>(true);
+      expect(item).toMatchObject({
+        isSuccess: false,
+        data: null,
+        isHit: false,
+        metadata: {
+          key: 'InvalidArgument for cacheKey (true)',
+        },
+      });
+      expect(item.error).toBeInstanceOf(InvalidCacheKeyException);
+      expect(item.error?.message).toStrictEqual('InvalidArgument for cacheKey (true)');
+    });
+
+    it('should indicate default message', () => {
+      const item = CacheItemFactory.fromInvalidCacheKey<string>(undefined);
+      expect(item.error).toBeInstanceOf(InvalidCacheKeyException);
+      expect(item.error?.message).toStrictEqual('InvalidArgument for cacheKey (undefined)');
+    });
+  });
+
+  describe('fromErr', () => {
+    it('should create an error item', () => {
+      const item = CacheItemFactory.fromErr({
+        key: 'k',
+        error: new CacheException({ message: 'cool' }),
+      });
+      expect(item).toMatchObject({
+        isSuccess: false,
+        data: null,
+        isHit: false,
+        metadata: {
+          key: 'k',
+        },
+      });
+      expect(item.error).toBeInstanceOf(CacheException);
+    });
+  });
+});
---
+++ packages/cache-interop/src/cache-item.factory.ts
@@ -0,0 +1,28 @@
+import { CacheItem, CacheItemPropsErr, CacheItemPropsOk } from './cache-item';
+import { InvalidCacheKeyException } from './exceptions';
+import { CacheKey } from './cache.interface';
+
+export class CacheItemFactory {
+  static fromCacheMiss<T, K extends CacheKey = CacheKey>(
+    props: Omit<CacheItemPropsOk<T | null, K>, 'success' | 'isHit' | 'data'> & {
+      defaultValue: T | null;
+      data?: never;
+    }
+  ): CacheItem<T | null, K> {
+    return new CacheItem<T | null, K>({ ...props, ...{ data: props.defaultValue, success: true, isHit: false } });
+  }
+  static fromOk<T, K extends CacheKey = CacheKey>(props: Omit<CacheItemPropsOk<T, K>, 'success'>): CacheItem<T, K> {
+    return new CacheItem<T, K>({ ...props, ...{ success: true } });
+  }
+  static fromErr<K extends CacheKey = CacheKey>(props: Omit<CacheItemPropsErr<K>, 'success'>): CacheItem<null, K> {
+    return new CacheItem<null, K>({ ...props, ...{ success: false } });
+  }
+  static fromInvalidCacheKey<K extends CacheKey = CacheKey>(key: unknown): CacheItem<null, string> {
+    const err = new InvalidCacheKeyException({ key });
+    return new CacheItem<null, string>({
+      success: false,
+      key: err.message,
+      error: err,
+    });
+  }
+}
