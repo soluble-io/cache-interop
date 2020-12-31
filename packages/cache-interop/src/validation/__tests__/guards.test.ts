c0622dcd941b92204b73150ea061e3c79e3eb8ea
Sébastien Vanvelthem <belgattitude@gmail.com>
2020-12-31 11:45:27 +0100

    feat: add isConnectedCache guard

--- packages/cache-interop/src/validation/__tests__/guards.test.ts
+++ packages/cache-interop/src/validation/__tests__/guards.test.ts
@@ -1,6 +1,32 @@
 import { Guards } from '../guards';
+import { CacheInterface } from '../../cache.interface';
+import { ConnectedCacheInterface } from '../../connection/connected-cache.interface';
 
 describe('Guards tests', () => {
+  describe('isConnectedCacheInterface', () => {
+    describe('when a CacheInterface is given', () => {
+      it('should return false', () => {
+        const object = {} as CacheInterface<unknown>;
+        expect(Guards.isConnectedCache(object)).toStrictEqual(false);
+      });
+    });
+    describe('when a ConnectedCacheInterface is given', () => {
+      it('should return true', () => {
+        const object = {
+          getConnection: () => {},
+        } as ConnectedCacheInterface<unknown>;
+        expect(Guards.isConnectedCache(object)).toStrictEqual(true);
+      });
+    });
+
+    describe('when unsupported types are given', () => {
+      it('should not throw and return false', () => {
+        expect(Guards.isConnectedCache(null as any)).toStrictEqual(false);
+        expect(Guards.isConnectedCache(undefined as any)).toStrictEqual(false);
+      });
+    });
+  });
+
   describe('isValidCacheKey', () => {
     it('should work as expected', () => {
       expect(Guards.isValidCacheKey('k')).toStrictEqual(true);
--- packages/cache-interop/src/validation/guards.ts
+++ packages/cache-interop/src/validation/guards.ts
@@ -1,8 +1,19 @@
-import { CacheKey } from '../cache.interface';
+import { CacheInterface, CacheKey } from '../cache.interface';
 import { isNonEmptyString } from '../utils';
+import { ConnectedCacheInterface } from '../connection/connected-cache.interface';
+
+type CacheOrConnectedCache = CacheInterface | ConnectedCacheInterface<unknown>;
 
 export class Guards {
   static isValidCacheKey<K extends CacheKey = CacheKey>(key: unknown): key is K {
     return isNonEmptyString(key);
+  }
+
+  /**
+   * Check whether a cache adapter implements ConnectedAdapterInterface
+   * and has a getConnection() method
+   */
+  static isConnectedCache<T = unknown>(adapter: CacheOrConnectedCache): adapter is ConnectedCacheInterface<T> {
+    return typeof ((adapter as unknown) as ConnectedCacheInterface<T>)?.getConnection === 'function';
   }
 }
