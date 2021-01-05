a455dd6657366afd722dc7f7a2f0992e52a147ae
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-01-05 16:14:43 +0100

    feat: add get in redis-async

--- packages/cache-redis/src/redis-async.util.ts
+++ packages/cache-redis/src/redis-async.util.ts
@@ -5,10 +5,11 @@ export type AsyncRedisClient = ReturnType<typeof getAsyncRedisClient>;
 
 export const getAsyncRedisClient = (redis: RedisClient) => {
   return {
-    flushdb: promisify(redis.flushdb).bind(redis),
+    get: promisify(redis.get).bind(redis),
     set: promisify(redis.set).bind(redis),
     setex: promisify(redis.setex).bind(redis),
     exists: promisify(redis.exists).bind(redis) as (k: string) => Promise<number>,
     del: promisify(redis.del).bind(redis) as (k: string) => Promise<number>,
+    flushdb: promisify(redis.flushdb).bind(redis),
   };
 };
