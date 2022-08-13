d79227c19c683175a9a0b72c61288b0345139252
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-08-13 13:11:08 +0200

    feat: prepare ci for example apps

--- examples/nextjs-cjs/next.config.js
+++ examples/nextjs-cjs/next.config.js
@@ -1,7 +1,23 @@
+const trueEnv = ['true', '1', 'yes'];
+
+const NEXTJS_IGNORE_ESLINT = trueEnv.includes(
+  process.env?.NEXTJS_IGNORE_ESLINT ?? 'false'
+);
+const NEXTJS_IGNORE_TYPECHECK = trueEnv.includes(
+  process.env?.NEXTJS_IGNORE_TYPECHECK ?? 'false'
+);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: NEXTJS_IGNORE_TYPECHECK,
+  },
+  eslint: {
+    ignoreDuringBuilds: NEXTJS_IGNORE_ESLINT,
+    dirs: ['src'],
+  },
 };
 
 module.exports = nextConfig;
--- examples/nextjs-esm/next.config.js
+++ examples/nextjs-esm/next.config.js
@@ -1,5 +1,21 @@
+const trueEnv = ['true', '1', 'yes'];
+
+const NEXTJS_IGNORE_ESLINT = trueEnv.includes(
+  process.env?.NEXTJS_IGNORE_ESLINT ?? 'false'
+);
+const NEXTJS_IGNORE_TYPECHECK = trueEnv.includes(
+  process.env?.NEXTJS_IGNORE_TYPECHECK ?? 'false'
+);
+
 /** @type {import('next').NextConfig} */
 export const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: NEXTJS_IGNORE_TYPECHECK,
+  },
+  eslint: {
+    ignoreDuringBuilds: NEXTJS_IGNORE_ESLINT,
+    dirs: ['src'],
+  },
 };
